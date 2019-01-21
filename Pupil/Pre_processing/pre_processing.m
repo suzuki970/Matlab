3c6d38c33b2895d13f92d5f6d2db8c1444f04228
Yuta Suzuki <yuta@eduroam-193-157-111-165.uio.no>
2019-01-21 14:43:42 +0100

    fix bugs

--- Pupil/Pre_processing/pre_processing.m
+++ Pupil/Pre_processing/pre_processing.m
@@ -2,17 +2,12 @@ function [ y rejctNum] = pre_processing( y,fs, thres, windowL, timeLen, method )
 
 %% ----------------------------------------------------
 
-rejctNum = [];
-for j = 1:size(y,1)
-    if sum(isnan(y(j,:))) > 0
-        rejctNum = [rejctNum;j];
-    end
-end
-
 %% filtering
 ave = mean(y,2);
 y = y - repmat(ave,1,size(y,2));
-% y = band_filter(y',fs,[0.005 35]);
+y = band_filter(y',fs,[0.001 35]);
+% y = band_filter(y',fs,[0.01 35]);
+% y = band_filter(y',fs,[0.1 35]);
 y = filter(ones(1,windowL)/windowL, 1, y')';
 y = y(:,windowL+1:end);
 y = y + repmat(ave,1,size(y,2));
@@ -22,14 +17,16 @@ startTime = timeLen(1);
 endTime = timeLen(2);
 
 x = [startTime:(endTime-startTime)/(size(y,2)-1):endTime];
-baselineData = [knnsearch(x',-0.2) knnsearch(x',0.0)];
+baselineData = [knnsearch(x',-0.2) knnsearch(x',0)];
 
 if method == 1
-y = y - repmat(median(y(:,baselineData(1):baselineData(2)),2),1,size(y,2));
+    y = y - repmat(median(y(:,baselineData(1):baselineData(2)),2),1,size(y,2));
 else
-  y = y ./ repmat(median(y(:,baselineData(1):baselineData(2)),2),1,size(y,2));
-end  
+    y = y ./ repmat(median(y(:,baselineData(1):baselineData(2)),2),1,size(y,2));
+end
+
 %% reject trials when the velocity of pupil change is larger than threshold
+rejctNum = [];
 FX = gradient(y);
 for j = 1:size(y,1)
     if ~isempty( find(abs(FX(j,baselineData(1):end)) > thres))
@@ -37,14 +34,19 @@ for j = 1:size(y,1)
     end
 end
 
+for j = 1:size(y,1)
+    if sum(isnan(y(j,:))) > 0
+        rejctNum = [rejctNum;j];
+    end
+end
+
 % for j = 1:size(y,1)
-%     if ~isempty( find(y(j,baselineData(1):end) < -1.5))
+%     if ~isempty( find(abs(y(j,baselineData(1):end)) > 1))
 %         rejctNum = [rejctNum;j];
 %     end
 % end
 
 rejctNum = unique(rejctNum);
-y(rejctNum,:)=[];
 
 
 
--- Pupil/Pre_processing/zeroInterp.m
+++ Pupil/Pre_processing/zeroInterp.m
@@ -4,66 +4,134 @@ for trials = 1:size(pupilData,1)
     
     zeroInd = find(pupilData(trials,:) == 0);
     
+    if length(zeroInd) > size(pupilData,2)/2
+        continue;
+    end
+   %     peakInd = find(gradient(pupilData(trials,:)) > 50);
+    %
+    %     zeroInd = unique([zeroInd peakInd]);
+    
     % if there are 0 value in the obtained data
     if length(zeroInd) > 0
         
-        %% return if 0 includes in the beginning or ending array
-        if zeroInd(1,1) < interval || zeroInd(1,end) > size(pupilData(trials,:),2) - interval
-%             return;
-        else
-             y = pupilData(trials,:);
-            % define the onset and offset of the eye blinks
-            diffOnOff = diff(zeroInd);
-            diffOnOff=[inf diffOnOff inf];
-            count = 1;
-            clear datOfblinkCood
-            datOfblinkCood = zeros(1,2);
-            for i = 2:length(diffOnOff)
-                if diffOnOff(i) >= interval && diffOnOff(i-1) >= interval      % oneshot noise
-                    datOfblinkCood(count,:) = [zeroInd(i-1) zeroInd(i-1)];
+        if zeroInd(1,1) == 1 && length(zeroInd) == 1
+            continue;
+        end
+        
+        if zeroInd(1,end) == size(pupilData(trials,:),2) && length(zeroInd) == 1
+            continue;
+        end
+        
+        if zeroInd(1,end) == size(pupilData(trials,:),2) && zeroInd(1,end) ~= zeroInd(1,end-1)+1
+            pupilData(trials,end) = pupilData(trials,end-1);
+            zeroInd(:,end) = [];
+        elseif zeroInd(1,end) > size(pupilData(trials,:),2) - interval
+            endFlag = true;
+            count = size(zeroInd,2);
+            rejInd = [];
+            while endFlag
+               if count == 1
+                    rejInd = [rejInd;count];
+                    break;
+                end
+            if zeroInd(1,count) == zeroInd(1,count-1)+1
+                    rejInd = [rejInd;count];
+                    count = count-1;
+                else
+                    rejInd = [rejInd;count];
+                    endFlag = false;
+                end
+                   
+            end
+            zeroInd(:,rejInd) = [];
+            %             zeroInd(:,end-interval:end) = [];
+        end
+    end
+    
+    if isempty(zeroInd)
+        continue;
+    end
+    %% return if 0 includes in the beginning or ending array
+    if zeroInd(1,1) < interval
+        %             || zeroInd(1,end) > size(pupilData(trials,:),2) - interval
+        %             zeroInd(:,1:interval) = [];
+        endFlag = true;
+        count = 1;
+        rejInd = [];
+        while endFlag
+            if count == size(zeroInd,2)
+                break;
+            end
+            if size(zeroInd,2) == 1
+                rejInd = [rejInd;count];
+                endFlag = false;
+            else
+                if zeroInd(1,count) == zeroInd(1,count+1)-1
+                    rejInd = [rejInd;count];
                     count = count+1;
-                elseif diffOnOff(i) >= interval && diffOnOff(i-1) <= interval  % offset of the blink
-                    datOfblinkCood(count,2) = zeroInd(i-1);
-                    count = count+1;
-                elseif diffOnOff(i) < interval && diffOnOff(i-1) < interval  % continuing blink
-                    
-                elseif diffOnOff(i) < interval && diffOnOff(i-1) >= interval  % onset of the blink
-                    datOfblinkCood(count,1)= zeroInd(i-1);
+                else
+                    rejInd = [rejInd;count];
+                    endFlag = false;
                 end
             end
+        end
+        zeroInd(:,rejInd) = [];
+    end
+    
+    %     else
+    y = pupilData(trials,:);
+    % define the onset and offset of the eye blinks
+    diffOnOff = diff(zeroInd);
+    diffOnOff=[inf diffOnOff inf];
+    count = 1;
+    %             clear datOfblinkCood
+    datOfblinkCood = zeros(1,2);
+    for i = 2:length(diffOnOff)
+        if diffOnOff(i) >= interval && diffOnOff(i-1) >= interval      % one-shot noise
+            datOfblinkCood(count,:) = [zeroInd(i-1) zeroInd(i-1)];
+            count = count+1;
+        elseif diffOnOff(i) >= interval && diffOnOff(i-1) <= interval  % offset of the blink
+            datOfblinkCood(count,2) = zeroInd(i-1);
+            count = count+1;
+        elseif diffOnOff(i) < interval && diffOnOff(i-1) < interval  % continuing blink
             
-            % adjust the onset and offset of the eye blinks
-            for i = 1:size(datOfblinkCood,1)
-                % for onset
-                while (y(datOfblinkCood(i,1)) - y(datOfblinkCood(i,1)-1)) <= 0
-                    datOfblinkCood(i,1) = datOfblinkCood(i,1)-1;
-                    if datOfblinkCood(i,1) == 1
-                        break;
-                    end
-                end
-                
-                % for offset
-                while (y(datOfblinkCood(i,2)) - y(datOfblinkCood(i,2)+1)) <= 0
-                    datOfblinkCood(i,2) = datOfblinkCood(i,2)+1;
-                    if datOfblinkCood(i,2) == size(y,2)
-                        break;
-                    end
-                end
+        elseif diffOnOff(i) < interval && diffOnOff(i-1) >= interval  % onset of the blink
+            datOfblinkCood(count,1)= zeroInd(i-1);
+        end
+    end
+    
+    % adjust the onset and offset of the eye blinks
+    for i = 1:size(datOfblinkCood,1)
+        % for onset
+        while (y(datOfblinkCood(i,1)) - y(datOfblinkCood(i,1)-1)) <= 0
+            datOfblinkCood(i,1) = datOfblinkCood(i,1)-1;
+            if datOfblinkCood(i,1) == 1
+                break;
             end
-            
-            for i = 1:size(datOfblinkCood,1)
-                onsetArray = datOfblinkCood(i,1);
-                offsetArray = datOfblinkCood(i,2);
-                
-                numX = [onsetArray offsetArray];
-                numY = pupilData(trials,numX);
-                
-                xx = onsetArray:offsetArray;
-                yy = interp1(numX,numY,xx,methods);
-                
-                pupilData(trials,xx) = yy;
+        end
+        
+        % for offset
+        while (y(datOfblinkCood(i,2)) - y(datOfblinkCood(i,2)+1)) <= 0
+            datOfblinkCood(i,2) = datOfblinkCood(i,2)+1;
+            if datOfblinkCood(i,2) == size(y,2)
+                break;
             end
         end
     end
+    
+    for i = 1:size(datOfblinkCood,1)
+        onsetArray = datOfblinkCood(i,1);
+        offsetArray = datOfblinkCood(i,2);
+        
+        numX = [onsetArray offsetArray];
+        numY = pupilData(trials,numX);
+        
+        xx = onsetArray:offsetArray;
+        yy = interp1(numX,numY,xx,methods);
+        
+        pupilData(trials,xx) = yy;
+    end
 end
+%     end
 end
+
