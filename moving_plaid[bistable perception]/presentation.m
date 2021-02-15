839552b57a43bd067c924eeb85603b897d6229dc
suzukiy970@gmail.com <suzukiy970@gmail.com>
2021-02-16 00:21:22 +0900

    add glare

--- moving_plaid[bistable perception]/presentation.m
+++ moving_plaid[bistable perception]/presentation.m
@@ -7,15 +7,24 @@ for iFlip = 1:60*cfg.PRESENTATION
     
     Screen('CopyWindow', window_s(condition_frame(count)),win);
     Screen('Flip', win,0,1);
-      
+    
+    imageArray=Screen('GetImage',win);
+    if iFlip < 10
+        imwrite(imageArray,['test00' num2str(iFlip) '.png']);
+    elseif iFlip > 9 && iFlip < 100
+        imwrite(imageArray,['test0' num2str(iFlip) '.png']);
+    else
+        imwrite(imageArray,['test' num2str(iFlip) '.png']);
+    end
+    
     count = count + 1;
-    if count > 60
+    if count > length(condition_frame)
         count = 1;
     end
     clear keyCode;
     [keyIsDown,secs,keyCode]=KbCheck;
     % interrupt by ESC
-    if (keyCode(escapeKey))
+    if (keyCode(cfg.KEYNAME.escapeKey))
         Screen('CloseAll');
         Screen('ClearAll');
         ListenChar(0);
